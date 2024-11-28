076bfa69d5334d7e08471589de8c5251653ed359
zzawook <kjaehyeok21@gmail.com>
2024-11-28 20:09:14 +0800

    Initialize Spring Boot project in be folder

---
+++ be/src/main/java/sg/toss_sg/TossSgApplication.java
@@ -0,0 +1,13 @@
+package sg.toss_sg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossSgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TossSgApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/sg/toss_sg/TossSgApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.toss_sg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossSgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
